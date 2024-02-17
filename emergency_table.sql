d11fcdbc2737171a2f91cadee838d2ef02343f33
mywebsiteinone <152523641+mywebsiteinone@users.noreply.github.com>
2024-02-17 18:56:48 +0800

    Add files via upload

---
+++ emergency_table.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 16, 2024 at 10:28 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hotline_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergency_table`
+--
+
+CREATE TABLE `emergency_table` (
+  `Home` varchar(225) NOT NULL,
+  `PNP` varchar(225) NOT NULL,
+  `BFP` varchar(225) NOT NULL,
+  `RHU` varchar(225) NOT NULL,
+  `MDRRMO` varchar(225) NOT NULL,
+  `PANELCO` varchar(225) NOT NULL,
+  `CONTACT` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
